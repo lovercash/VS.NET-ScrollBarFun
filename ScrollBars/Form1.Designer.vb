e74752d687ed76ea7b877afa58464e0ffed6755b
lovercash <31711416+lovercash@users.noreply.github.com>
2017-11-03 09:14:45 -0400

    Final
    Completed Project

---
+++ ScrollBars/Form1.Designer.vb
@@ -0,0 +1,297 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.hsbRed = New System.Windows.Forms.HScrollBar()
+        Me.lblRed = New System.Windows.Forms.Label()
+        Me.picSample = New System.Windows.Forms.PictureBox()
+        Me.lblBlue = New System.Windows.Forms.Label()
+        Me.hsbBlue = New System.Windows.Forms.HScrollBar()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblGreen = New System.Windows.Forms.Label()
+        Me.hsbGreen = New System.Windows.Forms.HScrollBar()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.label = New System.Windows.Forms.Label()
+        Me.hsbTextHeight = New System.Windows.Forms.HScrollBar()
+        Me.lblTextHeight = New System.Windows.Forms.Label()
+        Me.lblSampleText = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lblTextWidth = New System.Windows.Forms.Label()
+        Me.hsbTextWidth = New System.Windows.Forms.HScrollBar()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        CType(Me.picSample, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(11, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(27, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Red"
+        '
+        'hsbRed
+        '
+        Me.hsbRed.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.hsbRed.Location = New System.Drawing.Point(55, 48)
+        Me.hsbRed.Maximum = 264
+        Me.hsbRed.Name = "hsbRed"
+        Me.hsbRed.Size = New System.Drawing.Size(392, 20)
+        Me.hsbRed.TabIndex = 1
+        '
+        'lblRed
+        '
+        Me.lblRed.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblRed.AutoSize = True
+        Me.lblRed.Location = New System.Drawing.Point(450, 52)
+        Me.lblRed.Name = "lblRed"
+        Me.lblRed.Size = New System.Drawing.Size(27, 13)
+        Me.lblRed.TabIndex = 2
+        Me.lblRed.Text = "Red"
+        '
+        'picSample
+        '
+        Me.picSample.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.picSample.Location = New System.Drawing.Point(492, 48)
+        Me.picSample.Name = "picSample"
+        Me.picSample.Size = New System.Drawing.Size(91, 88)
+        Me.picSample.TabIndex = 6
+        Me.picSample.TabStop = False
+        '
+        'lblBlue
+        '
+        Me.lblBlue.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblBlue.AutoSize = True
+        Me.lblBlue.Location = New System.Drawing.Point(450, 120)
+        Me.lblBlue.Name = "lblBlue"
+        Me.lblBlue.Size = New System.Drawing.Size(28, 13)
+        Me.lblBlue.TabIndex = 9
+        Me.lblBlue.Text = "Blue"
+        '
+        'hsbBlue
+        '
+        Me.hsbBlue.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.hsbBlue.Location = New System.Drawing.Point(55, 116)
+        Me.hsbBlue.Maximum = 264
+        Me.hsbBlue.Name = "hsbBlue"
+        Me.hsbBlue.Size = New System.Drawing.Size(392, 20)
+        Me.hsbBlue.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(11, 120)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(28, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Blue"
+        '
+        'lblGreen
+        '
+        Me.lblGreen.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblGreen.AutoSize = True
+        Me.lblGreen.Location = New System.Drawing.Point(450, 86)
+        Me.lblGreen.Name = "lblGreen"
+        Me.lblGreen.Size = New System.Drawing.Size(36, 13)
+        Me.lblGreen.TabIndex = 12
+        Me.lblGreen.Text = "Green"
+        '
+        'hsbGreen
+        '
+        Me.hsbGreen.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.hsbGreen.Location = New System.Drawing.Point(55, 82)
+        Me.hsbGreen.Maximum = 264
+        Me.hsbGreen.Name = "hsbGreen"
+        Me.hsbGreen.Size = New System.Drawing.Size(392, 20)
+        Me.hsbGreen.TabIndex = 11
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(11, 86)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(36, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Green"
+        '
+        'label
+        '
+        Me.label.AutoSize = True
+        Me.label.Location = New System.Drawing.Point(12, 276)
+        Me.label.Name = "label"
+        Me.label.Size = New System.Drawing.Size(62, 13)
+        Me.label.TabIndex = 13
+        Me.label.Text = "Text Height"
+        '
+        'hsbTextHeight
+        '
+        Me.hsbTextHeight.Location = New System.Drawing.Point(86, 276)
+        Me.hsbTextHeight.Maximum = 228
+        Me.hsbTextHeight.Minimum = 4
+        Me.hsbTextHeight.Name = "hsbTextHeight"
+        Me.hsbTextHeight.Size = New System.Drawing.Size(167, 17)
+        Me.hsbTextHeight.TabIndex = 14
+        Me.hsbTextHeight.Value = 4
+        '
+        'lblTextHeight
+        '
+        Me.lblTextHeight.AutoSize = True
+        Me.lblTextHeight.Location = New System.Drawing.Point(264, 276)
+        Me.lblTextHeight.Name = "lblTextHeight"
+        Me.lblTextHeight.Size = New System.Drawing.Size(0, 13)
+        Me.lblTextHeight.TabIndex = 16
+        '
+        'lblSampleText
+        '
+        Me.lblSampleText.AutoSize = True
+        Me.lblSampleText.Location = New System.Drawing.Point(3, 4)
+        Me.lblSampleText.Name = "lblSampleText"
+        Me.lblSampleText.Size = New System.Drawing.Size(66, 13)
+        Me.lblSampleText.TabIndex = 17
+        Me.lblSampleText.Text = "Sample Text"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Panel1.Controls.Add(Me.lblSampleText)
+        Me.Panel1.Location = New System.Drawing.Point(299, 276)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(286, 261)
+        Me.Panel1.TabIndex = 18
+        '
+        'lblTextWidth
+        '
+        Me.lblTextWidth.AutoSize = True
+        Me.lblTextWidth.Location = New System.Drawing.Point(264, 309)
+        Me.lblTextWidth.Name = "lblTextWidth"
+        Me.lblTextWidth.Size = New System.Drawing.Size(0, 13)
+        Me.lblTextWidth.TabIndex = 21
+        '
+        'hsbTextWidth
+        '
+        Me.hsbTextWidth.Location = New System.Drawing.Point(86, 309)
+        Me.hsbTextWidth.Maximum = 255
+        Me.hsbTextWidth.Minimum = 3
+        Me.hsbTextWidth.Name = "hsbTextWidth"
+        Me.hsbTextWidth.Size = New System.Drawing.Size(167, 17)
+        Me.hsbTextWidth.TabIndex = 20
+        Me.hsbTextWidth.Value = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 309)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(59, 13)
+        Me.Label4.TabIndex = 19
+        Me.Label4.Text = "Text Width"
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(110, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(324, 24)
+        Me.Label2.TabIndex = 22
+        Me.Label2.Text = "Change Colors with the Scroll Bar"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label6
+        '
+        Me.Label6.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(82, 214)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(418, 24)
+        Me.Label6.TabIndex = 23
+        Me.Label6.Text = "Change the Text Position with the Scroll Bar"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(597, 549)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblTextWidth)
+        Me.Controls.Add(Me.hsbTextWidth)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.lblTextHeight)
+        Me.Controls.Add(Me.hsbTextHeight)
+        Me.Controls.Add(Me.label)
+        Me.Controls.Add(Me.lblGreen)
+        Me.Controls.Add(Me.hsbGreen)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.lblBlue)
+        Me.Controls.Add(Me.hsbBlue)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.picSample)
+        Me.Controls.Add(Me.lblRed)
+        Me.Controls.Add(Me.hsbRed)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.picSample, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents hsbRed As HScrollBar
+    Friend WithEvents lblRed As Label
+    Friend WithEvents picSample As PictureBox
+    Friend WithEvents lblBlue As Label
+    Friend WithEvents hsbBlue As HScrollBar
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblGreen As Label
+    Friend WithEvents hsbGreen As HScrollBar
+    Friend WithEvents Label5 As Label
+    Friend WithEvents label As Label
+    Friend WithEvents hsbTextHeight As HScrollBar
+    Friend WithEvents lblTextHeight As Label
+    Friend WithEvents lblSampleText As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents lblTextWidth As Label
+    Friend WithEvents hsbTextWidth As HScrollBar
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ ScrollBars/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub HsbRed_Scroll(sender As Object, e As ScrollEventArgs) Handles hsbRed.Scroll, hsbGreen.Scroll, hsbBlue.Scroll
+        lblRed.Text = hsbRed.Value
+        lblGreen.Text = hsbGreen.Value
+        lblBlue.Text = hsbBlue.Value
+        picSample.BackColor = Color.FromArgb(hsbRed.Value, hsbGreen.Value, hsbBlue.Value)
+    End Sub
+
+    Private Sub HsbTextHeight_Scroll(sender As Object, e As ScrollEventArgs) Handles hsbTextHeight.Scroll, hsbTextWidth.Scroll
+        lblTextHeight.Text = hsbTextHeight.Value
+        lblTextWidth.Text = hsbTextWidth.Value
+        lblSampleText.Location = New Point(hsbTextHeight.Value, hsbTextWidth.Value)
+    End Sub
+End Class
